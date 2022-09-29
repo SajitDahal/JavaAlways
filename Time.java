f68806d0c803302107519a14094989955adfefb8
SajitDahal <SajitDahal77@kbc.edu.np>
2022-09-29 12:27:19 +0545

    first commit

---
+++ Time.java
@@ -0,0 +1,9 @@
+//simple program to show current date in java
+import java.util.Date;
+
+public class Time {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
